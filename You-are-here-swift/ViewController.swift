6f04cd30c0bceb58ad9f72bccd43c482e218f458
Syu <syusan3@icloud.com>
2015-03-15 20:31:30 +0900

    You-are-here

---
+++ You-are-here-swift/ViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ViewController.swift
+//  You-are-here-swift
+//
+//  Created by 加藤 周 on 2015/03/15.
+//  Copyright (c) 2015年 mycompany. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+class ViewController: UIViewController,CLLocationManagerDelegate {
+    var myLocationManager:CLLocationManager!
+    
+    @IBOutlet weak var myLatitudeLabel: UILabel!
+    @IBOutlet weak var myLongitudeLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        // 現在地の取得.
+        myLocationManager = CLLocationManager()
+        
+        myLocationManager.delegate = self
+        
+        // セキュリティ認証のステータスを取得.
+        let status = CLLocationManager.authorizationStatus()
+        
+        // まだ認証が得られていない場合は、認証ダイアログを表示.
+        if(status == CLAuthorizationStatus.NotDetermined) {
+            println("didChangeAuthorizationStatus:\(status)");
+            // まだ承認が得られていない場合は、認証ダイアログを表示.
+            self.myLocationManager.requestAlwaysAuthorization()
+        }
+        
+        // 取得精度の設定.
+        myLocationManager.desiredAccuracy = kCLLocationAccuracyBest
+        // 取得頻度の設定.
+        myLocationManager.distanceFilter = 100
+
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        
+        println("didChangeAuthorizationStatus");
+        
+        // 認証のステータスをログで表示.
+        var statusStr = "";
+        switch (status) {
+        case .NotDetermined:
+            statusStr = "NotDetermined"
+        case .Restricted:
+            statusStr = "Restricted"
+        case .Denied:
+            statusStr = "Denied"
+        case .AuthorizedAlways:
+            statusStr = "Authorized"
+        case .AuthorizedWhenInUse:
+            statusStr = "AuthorizedWhenInUse"
+        }
+        println(" CLAuthorizationStatus: \(statusStr)")
+    }
+    
+    // ボタンイベントのセット.
+
+    
+    @IBAction func onClickMyButton(sender: AnyObject) {
+        myLocationManager.startUpdatingLocation()
+    }
+    // 位置情報取得に成功したときに呼び出されるデリゲート.
+    func locationManager(manager: CLLocationManager!,didUpdateLocations locations: [AnyObject]!){
+        
+        // 緯度・経度の表示.
+        myLatitudeLabel.text = "緯度：\(manager.location.coordinate.latitude)"
+        myLatitudeLabel.textAlignment = NSTextAlignment.Center
+        
+        myLongitudeLabel.text = "経度：\(manager.location.coordinate.longitude)"
+        myLongitudeLabel.textAlignment = NSTextAlignment.Center
+        
+        
+        self.view.addSubview(myLatitudeLabel)
+        self.view.addSubview(myLongitudeLabel)
+        
+    }
+    
+    // 位置情報取得に失敗した時に呼び出されるデリゲート.
+    func locationManager(manager: CLLocationManager!,didFailWithError error: NSError!){
+        print("error")
+    }
+    
+
+
+}
+
